edfd0e0949cd2fab7a11a2817d9cc69a2ff0c263
Steve <steve.densham@gmail.com>
2024-11-24 22:51:48 +0100

    fix: update null checks to use undefined for uploadTime, numColumns, and numRows

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,18 +2,19 @@
 	export let uploadTime: number | null;
 	export let numColumns: number | null;
 	export let numRows: number | null;
+	console.log(uploadTime, numColumns, numRows);
 </script>
 
 <footer class="flex w-full items-center justify-between bg-gray-800 p-4 text-white">
 	<div>
-		{#if uploadTime !== null}
+		{#if uploadTime !== undefined}
 			<p class="text-sm">
 				Upload and processing time: {uploadTime} seconds
 			</p>
 		{/if}
 	</div>
 	<div>
-		{#if numColumns !== null && numRows !== null}
+		{#if numColumns !== undefined && numRows !== undefined}
 			<p class="text-sm">
 				Variables: {numColumns}
 				Records: {numRows}
