58839d6c107e97b886566297ed73f3e9cc01e8b6
Steve <steve.densham@gmail.com>
2025-02-11 23:25:46 +0100

    scroll height

--- src/lib/components/VariableList.svelte
+++ src/lib/components/VariableList.svelte
@@ -54,7 +54,7 @@
 	);
 </script>
 
-<div class="px-3 py-2">
+<div class="max-h-[calc(100vh-14rem)] overflow-y-auto px-3 py-2">
 	<div class="space-y-1">
 		{#each sortedVariables as variable}
 			<!-- svelte-ignore a11y_no_static_element_interactions -->
