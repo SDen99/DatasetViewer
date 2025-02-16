bbb332e14322b66bee387c5c3baf56ea03f6fe07
Steve <steve.densham@gmail.com>
2025-02-16 18:20:23 +0100

    Configure Vite build to minify and remove console logs

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,26 @@ export default defineConfig({
 		fs: {
 			allow: ['static'] // Add static to allowed directories
 		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return 'vendor';
+					}
+				}
+			}
+		},
+
+		sourcemap: 'hidden',
+		minify: 'terser',
+		terserOptions: {
+			compress: {
+				drop_console: true, // removes all console.* calls
+				// or if you want to keep some:
+				pure_funcs: ['console.log', 'console.debug'] // only removes specific console methods
+			}
+		}
 	}
 });
